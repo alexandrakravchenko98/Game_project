b2f5576bff29c3ac2eddd880bd4cf508f0396d20
Александра Кравченко < >
2017-04-02 00:03:05 +0300

    1

--- src/GameBundle/Controller/DefaultController.php
+++ src/GameBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace GameBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use GameBundle\Entity\Records;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,9 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('GameBundle:Default:index.html.twig');
+    }
+    public function addRecord()
+    {
+        $records = new Records();
     }
 }
