8971b1db71cbf3410c67feefe0dc85a3974be5ee
toxic <tox1ctox@mail.ru>
2017-04-02 15:37:37 +0300

    pizdec

--- src/GameBundle/Controller/DefaultController.php
+++ src/GameBundle/Controller/DefaultController.php
@@ -6,15 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use GameBundle\Entity\Records;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Security("has_role('ROLE_USER')")
      */
     public function indexAction()
     {
-        return $this->render('GameBundle:Default:index.html.twig');
+        $user = $this->getUser();
+        return $this->render('GameBundle:Default:index.html.twig',array(''
+            . 'user' => $user));
     }
     public function addRecord()
     {
